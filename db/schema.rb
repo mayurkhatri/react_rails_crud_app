71a3444a290c275a872fc5f54643aa4400df6f8e
mayurkhatri <mayurkhatri122@gmail.com>
2019-04-11 15:36:35 +0530

    For Heroku - add pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_02_17_053848) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "event_type"
     t.date "event_date"
