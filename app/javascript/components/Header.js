4624a6d07766548f4825a2344709c77351427dee
mayurkhatri <mayurkhatri122@gmail.com>
2019-05-01 17:48:12 +0530

    Header - add link to home page

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Header = () => (
   <header>
-    <h1>Event Manager</h1>
+    <h1><Link to={`/events`}> Event Manager </Link></h1>
   </header>
 );
 
