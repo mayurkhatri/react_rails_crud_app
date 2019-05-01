fae249f8a99fd6da07adc65dde02f47c75443256
mayurkhatri <mayurkhatri122@gmail.com>
2019-05-01 16:54:52 +0530

    remove react-s-alert temporarily

--- app/javascript/helpers/notifications.js
+++ app/javascript/helpers/notifications.js
@@ -1,6 +1,3 @@
-import Alert from 'react-s-alert';
-import 'react-s-alert/dist/s-alert-default.css';
-import 'react-s-alert/dist/s-alert-css-effects/scale.css';
 
 const defaults = {
   position: 'top-right',
@@ -9,20 +6,14 @@ const defaults = {
   offset: 45,
 }
 
-export { Alert };
-
 export const success = (message, options = {}) => {
-  Alert.success(message, Object.assign(defaults, options));
 };
 
 export const info = (message, options = {}) => {
-  Alert.info(message, Object.assign(defaults, options));
 }
 
 export const warning = (message, options = {}) => {
-  Alert.warning(message, Object.assign(defaults, options));
 }
 
 export const error = (message, options = {}) => {
-  Alert.error(message, Object.assign(defaults, options));
 }
